e4c78dffe09f9e436ac1650192de79f99c75ef91
cdburner <davidwright66@googlemail.com>
2026-03-11 12:11:56 +0000

    Remove code
    Removed some code I was thinking about using for the New Input System, but didn't use in the end

--- Assets/Scripts/EyeStates/EyeController.cs
+++ Assets/Scripts/EyeStates/EyeController.cs
@@ -7,11 +7,6 @@ public class EyeController : MonoBehaviour
 {
     public Transform eye;
 
-    public InputAction upAction = new InputAction("upArrow", binding: "<Keyboard>/w");
-    public InputAction downAction = new InputAction("downArrow", binding: "<Keyboard>/s");
-    public InputAction leftAction = new InputAction("leftArrow", binding: "<Keyboard>/a");
-    public InputAction rightAction = new InputAction("rightArrow", binding: "<Keyboard>/d");
-
     
     public BaseEyeState currentState;
     public readonly CentreCentreState ccs = new CentreCentreState();
@@ -27,10 +22,6 @@ public class EyeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        upAction.Enable();
-        downAction.Enable();
-        leftAction.Enable();
-        rightAction.Enable();
         TransitionToState(ccs);
     }
 
